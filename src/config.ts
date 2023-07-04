2cec6636d0acd9c3c7b12350f38090e36701f990
Islam Hassan <ihassan@tripactions.com>
2023-07-05 00:12:08 +0200

    Configure website

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://iahvector.com",
+  author: "Islam Hassan",
+  desc: "Obviously, a blog",
+  title: "Islam A. Hassan",
+  ogImage: "og-banner.png",
   lightAndDarkMode: true,
   postPerPage: 3,
 };
@@ -13,42 +13,36 @@ export const SITE: Site = {
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
 
 export const LOGO_IMAGE = {
-  enable: false,
+  enable: true,
   svg: true,
-  width: 216,
+  width: 46,
   height: 46,
 };
 
 export const SOCIALS: SocialObjects = [
   {
+    name: "Mail",
+    href: "mailto:i@iahvector.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/iahvector",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
   {
-    name: "Facebook",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Facebook`,
-    active: true,
-  },
-  {
-    name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Instagram`,
-    active: true,
-  },
-  {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://linkedin.com/in/iahvector",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
   {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
+    name: "Flickr",
+    href: "https://www.flickr.com/photos/iahvector/",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: true,
   },
   {
     name: "Twitter",
