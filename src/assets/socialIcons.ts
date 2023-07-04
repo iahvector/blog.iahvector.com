87092ba5e84673c2de80cdc54834acfbdb46f00c
Islam Hassan <ihassan@tripactions.com>
2023-07-05 00:39:38 +0200

    fix flickr icon definition

--- src/assets/socialIcons.ts
+++ src/assets/socialIcons.ts
@@ -2,10 +2,11 @@ import type { SocialIcons } from "../types";
 
 const socialIcons: SocialIcons = {
   Flickr: `<svg
-    xmlns="http://www.w3.org/2000/svg
+    xmlns="http://www.w3.org/2000/svg"
     class="icon-tabler"
     stroke-linecap="round"
-    stroke-linejoin="round">
+    stroke-linejoin="round"
+  >
     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
     <path d="M 10.82,11.98
              C 10.82,13.99 9.18,15.63 7.17,15.63
